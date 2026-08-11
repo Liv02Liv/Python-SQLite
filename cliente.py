5174c8ebd661cc5c1b00fcae81df99ba9f9bf10d
Livia Rocha <liviarocha640@gmail.com>
2026-08-11 10:10:07 -0300

    Inserido-Valores

--- cliente.py
+++ cliente.py
@@ -4,9 +4,22 @@ conexao = sqlite3.connect("banco.db")
 cursor = conexao.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS pedidos (
+CREATE TABLE IF NOT EXISTS cliente (
                 id_Cliente INTEGER PRIMARY KEY AUTOINCREMENT,
                 Nome TEXT NOT NULL,
                 Cidade TEXT NOT NULL,
-                Idade Int
+                Idade INT 
                 )""")
+
+cursor.execute("""
+INSERT INTO cliente
+                (Nome, Cidade, Idade) 
+VALUES
+                ("Ana Souza", "São Paulo", 28),
+                ("Carlos Lima", "Curitiba", 35),
+                ("Fernada Alves", "São Paulo", 24),
+                ("Marcos Silva", "Recife", 41),
+                ("Juliana Rocha", "Curitiba", 31)
+                """)
+
+conexao.commit()
